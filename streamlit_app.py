5ac765a6b0b1a23a9065b95255cb8225af1132c0
gangulysg <ganguls@hotmail.com>
2025-01-26 14:47:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col ('FRUI
 st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect ('Choose upto 5 ingredients', my_dataframe, max_selections < 6)
 
-
 if ingredients_list:
       ingredients_string =''    
       for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ''
-            st.subheader(fruit_chosen + 'Nutrition Information')
+            #st.subheader(fruit_chosen + 'Nutrition Information')
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
             st_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
       #st.write(ingredients_string)
