c0660258889e3dc58eeed7bf1abea5d02b834adb
gangulysg <ganguls@hotmail.com>
2025-01-26 12:51:32 -0500

    rest streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ st.write('Name on smoothie will be :', name_on_order)
 ## st.write('You selected :', option)
 ##
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st_df = st.dataframe(smoothiefroot_response.json(), use_container_width=true)
-
+st_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col ('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
