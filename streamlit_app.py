06925aafd1311e708d4388bc4c92a51c75327e12
gangulysg <ganguls@hotmail.com>
2025-01-26 15:33:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,8 @@ ingredients_string =''
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col ('FRUIT_NAME'), col('SEARCH_ON')
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+st.stop()
+
 ingredients_list = st.multiselect ('Choose upto 5 ingredients', my_dataframe, max_selections = 5)
 
 if ingredients_list:
